26729ec025228d1ff78203e81426f0a9fccc6f7e
MAlngaawy <m.alngaawy@gmail.com>
2022-05-23 14:53:00 +0200

    interfaces

--- public/app.js
+++ public/app.js
@@ -22,3 +22,21 @@ invoices.push(invTwo);
 // invoices.push("Lol") // -------- CAN'T
 // invoices.push({ name: 'shaun' }); // -------- CAN'T
 console.log(invoices);
+const me = {
+    name: 'shaun',
+    //age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent ', amount);
+        return amount;
+    },
+};
+console.log(me);
+me.speak('hello, world');
+const greetPerson = (person) => {
+    console.log('hello ', person.name);
+};
+greetPerson(me);
+//greetPerson({name: 'shaun'}); // CAN'T
--- src/app.ts
+++ src/app.ts
@@ -34,3 +34,36 @@ invoices.push(invTwo);
 // invoices.push({ name: 'shaun' }); // -------- CAN'T
 
 console.log(invoices);
+
+/** Interfaces in Typescript */
+
+
+// interfaces
+export interface IsPerson { // it's like an object type stampa
+  name: string;
+  age?: number; // the ((?)) means it's an optional value 
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: 'shaun',
+  //age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount);
+    return amount;
+  },
+};
+
+console.log(me);
+me.speak('hello, world');
+
+const greetPerson = (person: IsPerson): void => {
+  console.log('hello ', person.name);
+}
+
+greetPerson(me);
+//greetPerson({name: 'shaun'}); // CAN'T
